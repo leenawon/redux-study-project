83d9fe8978a155ae9956527288d19cba31f4c176
leenawon <nawon5154@gmail.com>
2022-01-02 02:27:01 +0900

    mapDispatchToProps 이용해서 삭제 버튼 클릭 시 삭제되도록 기능 추가

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,17 +1,25 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { storeActions } from '../reduxStore';
 
-function ToDo({todo, id}) {
+function ToDo({todo, id, deleteTodo}) {
   return (
     <div>
       <li>
         <Link to={`/${id}`}>
           {todo}
         </Link>
-        <button type="button">삭제</button>
+        <button type="button" onClick={deleteTodo}>삭제</button>
       </li>
     </div>
   )
 }
 
-export default ToDo;
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    deleteTodo: () => dispatch(storeActions.deleteAction(ownProps.id))
+  };
+}
+
+export default connect(null, mapDispatchToProps)(ToDo);
