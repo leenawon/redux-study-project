40025f264c5ff183377805c8ba994124c084ca1b
leenawon <nawon5154@gmail.com>
2022-01-03 00:45:34 +0900

    localStorage를 이용해서 데이터를 저장, 삭제 할 수 있도록 reducer와 action 수정

--- src/reduxStore.js
+++ src/reduxStore.js
@@ -4,23 +4,37 @@ import { createStore } from 'redux';
 const ADD_TODO = "ADD_TODO";
 const DELETE_TODO = "DELETE_TODO";
 
+// localStorage
+const toDoArray = "toDoArray";
+// localStorage에 null인 경우 발생하는 오류를 방지하기 위함
+if(localStorage.getItem(toDoArray) === null) {
+  // toDoArray 이름으로 빈 배열을 저장해줌
+  localStorage.setItem(toDoArray, JSON.stringify([]));
+}
+
 // add action (action은 reducer에게 전달하고자하는 메시지를 담음)
 const addAction = (todo) => {
-  return {type: ADD_TODO, todo, id: Date.now()};
+  return {type: ADD_TODO, todo, id: Date.now(), getToDoArray: JSON.parse(localStorage.getItem(toDoArray))};
 };
 
 // delete action
 const deleteAction = (id) => {
-  return {type: DELETE_TODO, id};
+  return {type: DELETE_TODO, id, getToDoArray: JSON.parse(localStorage.getItem(toDoArray))};
 };
 
 // redux reducer
 const reduxReducer = (state = [], action) => {
+  let getTodo = action.getToDoArray;
   switch (action.type) {
     case ADD_TODO:
-      return [{todo: action.todo, id: action.id}, ...state];
+      // concat() : 기존 배열에 추가하려는 배열을 합쳐서 새로운 배열을 반환
+      const addToDoArray = getTodo.concat({todo: action.todo, id: action.id});
+      localStorage.setItem(toDoArray, JSON.stringify(addToDoArray));
+      return addToDoArray;
     case DELETE_TODO:
-      return state.filter((todo) => todo.id !== action.id);
+      const deleteToDoArray = getTodo.filter((todo) => todo.id !== action.id);
+      localStorage.setItem(toDoArray, JSON.stringify(deleteToDoArray));
+      return deleteToDoArray;
     default:
       return state;
   }
