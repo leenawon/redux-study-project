76bfcadfd3f93177ef833ea5c4e252ddfd6dc925
leenawon <nawon5154@gmail.com>
2022-01-08 00:07:23 +0900

    redux toolkit의 createAction 함수 적용해서 action 부분 수정

--- src/reduxStore.js
+++ src/reduxStore.js
@@ -1,39 +1,47 @@
+import { createAction } from '@reduxjs/toolkit';
 import { createStore } from 'redux';
 
 // action constant
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
+// const ADD_TODO = "ADD_TODO";
+// const DELETE_TODO = "DELETE_TODO";
 
 // localStorage
-const toDoArray = "toDoArray";
+const TODOARRAY_KEY = "toDoArray";
 // localStorage에 null인 경우 발생하는 오류를 방지하기 위함
-if(localStorage.getItem(toDoArray) === null) {
+if(localStorage.getItem(TODOARRAY_KEY) === null) {
   // toDoArray 이름으로 빈 배열을 저장해줌
-  localStorage.setItem(toDoArray, JSON.stringify([]));
+  localStorage.setItem(TODOARRAY_KEY, JSON.stringify([]));
 }
 
 // add action (action은 reducer에게 전달하고자하는 메시지를 담음)
-const addAction = (todo) => {
-  return {type: ADD_TODO, todo, id: Date.now(), getToDoArray: JSON.parse(localStorage.getItem(toDoArray))};
-};
+// const addAction = (todo) => {
+//   return {type: ADD_TODO, todo, id: Date.now(), getToDoArray: JSON.parse(localStorage.getItem(toDoArray))};
+// };
+
+// Redux toolkit createAction 함수 적용
+const addAction = createAction("ADD_TODO");
 
 // delete action
-const deleteAction = (id) => {
-  return {type: DELETE_TODO, id, getToDoArray: JSON.parse(localStorage.getItem(toDoArray))};
-};
+// const deleteAction = (id) => {
+//   return {type: DELETE_TODO, id, getToDoArray: JSON.parse(localStorage.getItem(toDoArray))};
+// };
+
+const deleteAction = createAction("DELETE_TODO");
 
 // redux reducer
 const reduxReducer = (state = [], action) => {
-  let getTodo = action.getToDoArray;
+  let getTodo;
   switch (action.type) {
-    case ADD_TODO:
+    case addAction.type:
+      getTodo = JSON.parse(localStorage.getItem(TODOARRAY_KEY));
       // concat() : 기존 배열에 추가하려는 배열을 합쳐서 새로운 배열을 반환
-      const addToDoArray = getTodo.concat({todo: action.todo, id: action.id});
-      localStorage.setItem(toDoArray, JSON.stringify(addToDoArray));
+      const addToDoArray = getTodo.concat({todo: action.payload, id: Date.now()});
+      localStorage.setItem(TODOARRAY_KEY, JSON.stringify(addToDoArray));
       return addToDoArray;
-    case DELETE_TODO:
-      const deleteToDoArray = getTodo.filter((todo) => todo.id !== action.id);
-      localStorage.setItem(toDoArray, JSON.stringify(deleteToDoArray));
+    case deleteAction.type:
+      getTodo = JSON.parse(localStorage.getItem(TODOARRAY_KEY));
+      const deleteToDoArray = getTodo.filter((todo) => todo.id !== action.payload);
+      localStorage.setItem(TODOARRAY_KEY, JSON.stringify(deleteToDoArray));
       return deleteToDoArray;
     default:
       return state;
